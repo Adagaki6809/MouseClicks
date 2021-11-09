310881e242575ccad6076cf7a66a0ed377269beb
Adagaki6809 <include.ofstream.h@gmail.com>
2021-11-09 14:07:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+class InterceptKeys
+{
+    private const int WH_MOUSE_LL = 14;
+    private const int WM_LBUTTONDOWN = 0x0201;
+    private static readonly LowLevelMouseProc _proc = HookCallback;
+    private static IntPtr _hookID = IntPtr.Zero;
+    private static readonly Stopwatch sw = new Stopwatch();
+    private static int i = 1;
+
+    public static void Main()
+    {
+        _hookID = SetHook(_proc);
+        Application.Run();
+        UnhookWindowsHookEx(_hookID);
+    }
+
+    private static IntPtr SetHook(LowLevelMouseProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule)
+        {
+            return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && wParam == (IntPtr)WM_LBUTTONDOWN)
+        {
+            if (sw.IsRunning)
+            {
+                sw.Stop();
+                if (sw.ElapsedMilliseconds > 1000)
+                {
+                    sw.Reset();
+                    Console.WriteLine("....................Время между кликами больше 1 секунды......................");
+                    i = 1;
+                }
+                else
+                {
+                    Console.WriteLine("[{0,2}] Время:{1,4} мс", i, sw.ElapsedMilliseconds);
+                    i++;
+                    sw.Reset();
+                }
+            }
+
+            if (!sw.IsRunning)
+                sw.Start();
+        }
+
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook,
+        LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+        IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("User32.dll")]
+    static extern void mouse_event(MouseFlags dwFlags, int dx, int dy, int dwData, UIntPtr dwExtraInfo);
+
+    [Flags]
+    enum MouseFlags
+    {
+        Move = 0x0001, LeftDown = 0x0002, LeftUp = 0x0004, RightDown = 0x0008,
+        RightUp = 0x0010, Absolute = 0x8000
+    };
+
+}
+
+
+//flag = 1;
+//Console.WriteLine("MouseDown[{0}]\n",i);
+//i++;
+
+
+
+//int vkCode = Marshal.ReadInt32(lParam);
+//(Keys)vkCode
